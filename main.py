6ca08015b907c99ebe87010b498ea4dbea06b97c
vselected <dominik.sarleja@gmail.com>
2024-11-21 13:58:00 +0100

    Create welcome page and function that takes user choice

---
+++ main.py
@@ -0,0 +1,17 @@
+# Create Function Called Calculator
+def calculator():
+    # Welcome user and explain how the program works
+    print("Welcome to the Calculator!")
+    print("Select an operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    print("\n")
+
+    # Get user choice
+    choice = input("Enter the number of the operation you want to perform (1/2/3/4): ")
+
+
+# Run Calculator
+calculator()
